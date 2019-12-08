3f8d29d09b69a60c916620cc813c504078573cd2
Rodrigues <Rodrigues@LAPTOP-T798OTG1>
2019-12-09 00:17:54 +0530

    Tree code added for dataStructures

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module dsa {
+	exports circularqueuearray;
+	exports node;
+	exports singlelinkedlist;
+	exports circulardoublelinkedlist;
+	exports circularsinglelinkedlist;
+	exports linearqueuearray;
+	exports stacklinkedlist;
+	exports linearqueuelinkedlist;
+	exports doublelinkedlist;
+	exports stackarray;
+}
