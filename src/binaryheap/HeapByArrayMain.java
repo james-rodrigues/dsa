02677da1b84bbe27695c7f6ae19a115fd21584bb
Rodrigues <Rodrigues@LAPTOP-T798OTG1>
2019-12-09 22:50:31 +0530

    Binary Heap Examples added

---
+++ src/binaryheap/HeapByArrayMain.java
@@ -0,0 +1,33 @@
+package binaryheap;
+
+/**
+ * The Class HeapByArrayMain.
+ */
+public class HeapByArrayMain {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Creating a blank Heap");
+		HeapByArray heap = new HeapByArray(10);
+
+		heap.insertInHeap(100);
+		heap.insertInHeap(90);
+		heap.insertInHeap(80);
+		heap.insertInHeap(70);
+		heap.insertInHeap(60);
+		heap.insertInHeap(50);
+		heap.insertInHeap(40);
+		heap.insertInHeap(30);
+		heap.insertInHeap(20);
+
+		heap.extractHeadOfHeap();
+
+		heap.insertInHeap(110);
+		heap.extractHeadOfHeap();
+
+	}
+}// end of class
